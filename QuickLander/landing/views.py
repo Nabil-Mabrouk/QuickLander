b3c68f0f71bdbabddf5429514525170d43da6f96
Nabil-Mabrouk <nabil.mabrouk.ai@gmail.com>
2023-12-29 00:55:16 +0100

    version 1

---
+++ QuickLander/landing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+# landing/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'landing/index.html')
