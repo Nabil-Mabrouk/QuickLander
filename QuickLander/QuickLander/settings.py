b356c959633d9455a23af797afae94d15226aa30
Nabil-Mabrouk <nabil.mabrouk.ai@gmail.com>
2023-12-29 07:18:45 +0100

    version 1 no nginx bug3

--- QuickLander/QuickLander/settings.py
+++ QuickLander/QuickLander/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
-STATIC_ROOT='/usr/src/app/staticfiles/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/') # new
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
